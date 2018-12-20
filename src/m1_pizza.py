0c39122787a6081ad37f8ee12d7519e4af8517bd
bobbyliu6 <45321304+bobbyliu6@users.noreply.github.com>
2018-12-20 16:17:19 -0500

    DONE

--- src/m1_pizza.py
+++ src/m1_pizza.py
@@ -31,8 +31,8 @@ def main():
     # -------------------------------------------------------------------------
 
     # run_test_generate_points_on_circle()
-    run_test_draw_points_on_circle()
-    # run_test_pizza()
+    # run_test_draw_points_on_circle()
+    run_test_pizza()
     # run_test_polygon()
     # run_test_fancy_polygon()
 
@@ -152,7 +152,7 @@ def generate_points_on_circle(circle_for_points, number_of_points_to_generate):
 def run_test_draw_points_on_circle():
     """ Tests the   draw_points_on_circle   function. """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this TEST function.
+    # DONE: 3. Implement this TEST function.
     #   It TESTS the   draw_points_on_circle   function defined below.
     #   Include at least ** 1 ** ADDITIONAL test (that YOU write).
     #
@@ -259,7 +259,7 @@ def draw_points_on_circle(window, circle, number_of_points, color):
 def run_test_pizza():
     """ Tests the   pizza   function. """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement this TEST function.
+    # DONE: 5. Implement this TEST function.
     #   It TESTS the   pizza   function defined below.
     #   Include at least ** 1 ** ADDITIONAL test (that YOU write).
     #
@@ -303,8 +303,26 @@ def run_test_pizza():
     #     -- on a yellow-filled circle.
     # -------------------------------------------------------------------------
 
+    # Test 4:
+    title = 'PIZZA test 4:  10 slices, thin (thickness=2) black lines.'
+    window = rg.RoseWindow(400, 400, title)
+    circle = rg.Circle(rg.Point(200, 200), 150)
+    circle.outline_thickness = 2
+    pizza(window, circle, 100, 'black', 2)
+    window.close_on_mouse_click()
+
+
 
 def pizza(window, circle, number_of_slices, color, thickness):
+    circle.attach_to(window)
+    points=generate_points_on_circle(circle,number_of_slices)
+    for k in range(len(points)):
+        line=rg.Line(points[k],circle.center)
+        line.color=color
+        line.thickness=thickness
+        line.attach_to(window)
+        window.render(0.05)
+
     """
     What comes in:
       -- an rg.RoseWindow
@@ -334,7 +352,7 @@ def pizza(window, circle, number_of_slices, color, thickness):
       :type thickness:        int
     """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # IMPLEMENTATION REQUIREMENT:
